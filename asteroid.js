9df5fa5a684aae99bfd9b66fdb81e5d2ca293cfd
Oliver Wright <meiamsome@meiamso.me>
2016-12-17 01:38:40 +0000

    Attempt to fix collision issues

--- asteroid.js
+++ asteroid.js
@@ -68,23 +68,6 @@ function Asteroid(pos, r, size) {
 
     return vertices;
   }
-
-  this.verticesFromAngles = function(first_angle, second_angle) {
-    var angle_difference = atan2(sin(second_angle-first_angle), cos(second_angle-first_angle));
-    if(angle_difference < 0) return this.verticesFromAngles(second_angle, first_angle);
-    var lowest_node = floor(map(first_angle - this.heading, -TWO_PI, TWO_PI, 0, 2*this.total));
-    var highest_node = ceil(map(second_angle - this.heading, -TWO_PI, TWO_PI, 0, 2*this.total));
-    if(highest_node < lowest_node) highest_node += this.total;
-
-    var vertices = []
-    for(var i = lowest_node; i <= highest_node; i++) {
-      var angle = this.heading + map(i % this.total, 0, this.total, 0, TWO_PI);
-      var r = this.r + this.offset[i % this.total];
-      vertices.push(createVector(r * cos(angle), r * sin(angle)).add(this.pos));
-    }
-
-    return vertices;
-  }
 }
 
 Asteroid.prototype = Object.create(Entity.prototype);
